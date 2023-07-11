e24e33053122fbeee2b594a8236b4541cf3ddb64
The-Flying-Dev <leandranm@outlook.com>
2023-07-11 17:20:00 +0200

    Password digest and Sessions controller

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_07_10_111927) do
+ActiveRecord::Schema.define(version: 2023_07_13_232244) do
 
   create_table "comments", force: :cascade do |t|
     t.text "body"
@@ -47,6 +47,7 @@ ActiveRecord::Schema.define(version: 2023_07_10_111927) do
     t.string "email"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "password_digest"
   end
 
   add_foreign_key "comments", "posts"
